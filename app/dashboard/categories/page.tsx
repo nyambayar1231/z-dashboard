27622edf522897d5dd8b89a6294a591aa0b9bc34
Nyambayar1231 <nyambayarlucky@gmail.com>
2024-01-04 15:41:47 +0800

    remove form to do fix

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -1,5 +1,5 @@
 import { lusitana } from '@/app/ui/fonts';
-// import { CreateCategory } from '@/app/ui/categories/buttons';
+import { CreateCategory } from '@/app/ui/categories/buttons';
 import Search from '@/app/ui/search';
 import { Suspense } from 'react';
 import { Metadata } from 'next';
@@ -32,7 +32,7 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Ангилал хайх" />
-        {/* <CreateCategory /> */}
+        <CreateCategory />
       </div>
       <Suspense
         key={query + currentPage}
